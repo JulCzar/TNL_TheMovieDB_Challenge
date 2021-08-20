c6c161290b2f6b3f8bb5249e1819dae4ee0288a3
JulCzar <julczar21@outlook.com>
2021-08-20 10:44:11 -0300

    :sparkles: removed global style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
-import { Spinner } from '@chakra-ui/react'
+import { Button, Container } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Spinner
-        thickness='4px'
-        speed='1s'
-        emptyColor='gray.400'
-        color='blue.500'
-        size='xl'
-      />
-    </div>
+    <Container>
+      <Button>Clique</Button>
+    </Container>
   )
 }
 
